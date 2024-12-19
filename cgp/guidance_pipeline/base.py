3da797a14128635d92a1581a195b07c01acae640
Mehant Kammakomati <mehant.kammakomati2@ibm.com>
2024-12-19 13:28:05 +0530

    nomsg
    Signed-off-by: Mehant Kammakomati <mehant.kammakomati2@ibm.com>

--- cgp/guidance_pipeline/base.py
+++ cgp/guidance_pipeline/base.py
@@ -10,26 +10,11 @@ model = sys.argv[1]
 tokenizer_base = AutoTokenizer.from_pretrained(model)
 model_base = AutoModelForCausalLM.from_pretrained(model, torch_dtype=torch.float16, device_map="auto")
 
-
-
-
-# for i in tqdm(range(0, len(ir_data_df))):
-
-module_list = "['ansible.builtin.file', 'ansible.builtin.command']"
-prompt = "- name: Change file ownership, group and permissions"
-module_line = prompt.split("\n")[-1]
-module_spaces = " "*(cnt_spaces_left(prompt) + 2)
-
-if prompt.startswith("name"):
-  module_spaces = ""
-else:
-  module_spaces = " "*(cnt_spaces_left(prompt) + 2)
+prompt = "- name: Change file ownership, group and permissions\n"
 
 
 try:
-  # prog_module = guidance(module_selection_prompt, token_healing=True, llm=model)
-  # module_opt = prog_module(module_list=reference_module)
-  final_prompt = prompt + "\n" + module_spaces + "ansible.builtin.file" + ":\n" + module_spaces + " "
+  final_prompt = prompt
   input_ids = tokenizer_base(final_prompt, return_tensors="pt").input_ids.to("cuda")
   gen_tokens = model_base.generate(input_ids, num_beams=5, num_return_sequences=1, max_new_tokens=150)
   gen_tokens = gen_tokens.reshape(1, -1, gen_tokens.shape[-1])[0][0]
